ad0d3462679f17174f9e1e22b6bd637b19cf6ae2
T-Sumida <ui.suto05@gmail.com>
2021-05-29 22:11:31 +0900

    [update] update

--- model_creator.py
+++ model_creator.py
@@ -2,11 +2,10 @@
 
 # モデル作成用スクリプト
 
-import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import ResNet50
 
 if __name__ == "__main__":
     model = ResNet50(weights='imagenet')
     print(model.summary())
-    model.save("./static/model/resnet_imagenet.h5")
-    print("model output to \"static/model/resnet_imagenet.h5\"")
+    model.save("./models/")
+    print("model output to \"./models\"")
